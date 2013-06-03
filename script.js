7a6d0e73556b8e9d420dce90fcc4aff14c7ab118
Megan Kanne <mkanne@cs.stanford.edu>
2013-06-02 18:58:04 -0700

    added icons; sunburst and drag and select now working (mostly)

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 //Constants
-var width = 320;
-var height = 356; //356
+var width = Math.min(window.screen.availWidth, 1000); //320
+var height = Math.min(window.screen.availHeight, 1400); //356
 var margin = {top: 20, right: 10, bottom: 20, left: 10};
 var formh = height-margin.top-margin.bottom;
+// var formw = Math.min(width-margin.right-margin.left, 400);
 var formw = width-margin.right-margin.left;
 var submith = 36 + 10;
 var texth = 96;
 var totalh = texth + submith;
 var cdiameter = 70;
-var radius = (Math.max(width, height) + 200) / 2;
+var radius = Math.max(width, height);
 var color = d3.scale.category20c();
-var tags = ["tag1", "tag2", "tag3"];
+var tags = ["polarbear dog", "lion turtle", "turtle duck", "eel hound", "koala otter"];
+var drag = d3.behavior.drag()
+	.origin(Object)
+	.on("drag", dragmove)
+	.on("dragend", dragend);
+var arc = d3.svg.arc()
+    .outerRadius(radius)
+    .innerRadius(80);
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return 1; });
 
 //Setup
+d3.select("svg")
+	.attr("width", width)
+	.attr("height", height);
+d3.select("#container")
+	.style("width", function(){return width + "px";})
+	.style("height", function(){return height + "px";});
 d3.select("rect")
 	.attr("x", margin.left)
 	.attr("y", height/2 - totalh/2)
@@ -27,24 +44,15 @@ d3.select("#formobject")
 	.attr("width", formw)
 	.attr("height", 356);
 d3.select("#note")
-	.style("height", function(){return texth + "px";});
+	.style("height", function(){return texth + "px";})
+	.style("width", function(){return formw-6 + "px";});
 	
 	
 //Variables
 var note; //the text area content
-var drag = d3.behavior.drag()
-	.origin(Object)
-	.on("drag", dragmove);
-// var partition = d3.layout.partition()
-//     .sort(null)
-//     .size([2 * Math.PI, radius * radius])
-//     .value(function(d) { return 1; });
-var arc = d3.svg.arc()
-    .outerRadius(radius)
-    .innerRadius(80);
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return 1; });
+var tag; //the selected tag
+var dragdx = 0;
+var dragdy = 0;
 
 function handleClick(event){
 	note = document.getElementById("note").value;
@@ -81,7 +89,7 @@ function transition(){
 		.delay(100)
 		.duration(1000)
 			.attr("width", cdiameter)
-			.attr("height", cdiameter)
+			.attr("height", cdiameter)			
 			.attr("x", (width/2) - (cdiameter/2))
 			.attr("y", (height/2) - (cdiameter/2));
 	
@@ -98,8 +106,14 @@ function transition(){
 		.attr("d", arc)
 		.style("fill", function(d, i) { return color(i); });
 	g.append("text")
-		.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-		.attr("dy", ".35em")
+		.attr("transform", function(d) { 
+			var angle = ((180*(d.startAngle+d.endAngle)/2)/Math.PI)-90;
+			var newy = arc.centroid(d)[1]/2;
+			var newx = arc.centroid(d)[0]/2;
+			return "translate("+[newx, newy]+")rotate("+angle+")rotate("+(angle>90?-180:0)+")"; 
+		})
+		.attr("text-anchor", "start")
+		.attr("dy", "-.2em")
 		.style("text-anchor", "middle")
 		.text(function(d) { return d.data; });
 	g.transition()
@@ -110,10 +124,79 @@ function transition(){
 }
 
 function dragmove(d) {
-	console.log("drag event");
-	console.log(d);
-	console.log(d3.event);
+	// console.log("drag event");
+	// console.log(d);
+	// console.log(d3.event);
+	dragdx += d3.event.dx;
+	dragdy += d3.event.dy;
 	d3.select(this)
-		.attr("x", d.x = Math.max(0, Math.min(width - 44, d3.event.x)))
-		.attr("y", d.y = Math.max(0, Math.min(height - 44, d3.event.y)));
+		.attr("x", d.x = Math.max(0, Math.min(width - cdiameter, d3.event.x)))
+		.attr("y", d.y = Math.max(0, Math.min(height - cdiameter, d3.event.y)));
+}
+
+function dragend(d) {
+	// console.log("drag end");
+	// console.log(d);
+	// console.log(d3.event);
+	// console.log(dragdx+", "+dragdy);
+	// TO DO: set tag
+	var direction = Math.min(Math.abs(dragdx), Math.abs(dragdy));
+	// console.log(d.x+", "+d.y);
+	// console.log(d3.event.sourceEvent.x+", "+d3.event.sourceEvent.y);	
+	//tag = d3.select(document.elementFromPoint(d3.event.sourceEvent.x+Math.min((width/5) * (dragdx/direction), width/3), d3.event.sourceEvent.y+Math.min((width/5) * (dragdx/direction), width/3)))[0][0].__data__.data;
+	//console.log(document.elementFromPoint(d3.event.sourceEvent.x+Math.min((width/5) * (dragdx/direction), width/3), d3.event.sourceEvent.y+Math.min((width/5) * (dragdx/direction), width/3)));
+	//console.log(tag);
+	d3.select(this).transition()
+		.duration(3000)
+		.ease("cubic-out")
+		.attr("x", d.x += Math.min((width/5) * (dragdx/direction), width/5))
+		.attr("y", d.y += Math.min((height/5) * (dragdy/direction), height/5));
+	//console.log(Math.min((width/5) * (dragdx/direction), width/3) + ", " + Math.min((width/5) * (dragdy/direction), width/3));
+	tag = d3.select(document.elementFromPoint(d3.event.sourceEvent.x+40, d3.event.sourceEvent.y+40))[0][0].__data__.data;
+	// console.log(tag);
+	// console.log((width/5) * (dragdx/direction)+", "+(height/5) * (dragdy/direction));
+	// console.log(Math.min((width/5) * (dragdx/direction), width/4)+", "+Math.min((height/5) * (dragdy/direction), height/4));
+	dragdx = 0;
+	dragdy = 0;
+	fadeoutall();
+	feedback();
+}
+
+function fadeoutall() {
+	var svg = d3.selectAll("g")
+		.transition()
+		.delay(3000)
+		.duration(2000)
+			.style("opacity", 0);
+	svg.transition().delay(5100)
+		.remove();
+	var rect = d3.selectAll("rect")
+		.transition()
+		.delay(3000)
+		.duration(2000)
+			.style("opacity", 0);
+	rect.transition().delay(5100)
+		.remove();
+	d3.select("svg")
+		.transition().delay(5100)
+		.remove();
+	
+}
+
+function feedback() {
+	setTimeout(function (){
+        var container = d3.select("#container");
+		var div = container.append("div")
+			.style("color", "white")
+			.attr("class", "feedback")
+			.style("margin-top", function(){return (height/2)-100 + "px";})
+			.style("width", function(){return Math.max(width/4, 200) + "px";});
+		div.append("p")
+			.text("New note created: \""+note+"\" and tagged \""+tag+"\"");
+		div.append("a")
+			.attr("href", "index.html")
+			.attr("class", "again")
+			.text("New Note");
+    }, 5200);
+
 }
