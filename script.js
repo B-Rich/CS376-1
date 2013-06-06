649ea32dd9b1b2ce8857949559f1e9e00696eda0
Megan Kanne <mkanne@cs.stanford.edu>
2013-06-06 12:59:03 -0700

    deleted debugging code

--- script.js
+++ script.js
@@ -124,9 +124,6 @@ function transition(){
 }
 
 function dragmove(d) {
-	// console.log("drag event");
-	// console.log(d);
-	// console.log(d3.event);
 	dragdx += d3.event.dx;
 	dragdy += d3.event.dy;
 	d3.select(this)
@@ -135,26 +132,7 @@ function dragmove(d) {
 }
 
 function dragend(d) {
-	// console.log("drag end");
-	console.log(d);
-	console.log(d3.event);
-	// console.log(dragdx+", "+dragdy);
-	// TO DO: set tag
 	var direction = Math.min(Math.abs(dragdx), Math.abs(dragdy));
-	console.log("d.x,d.y: "+d.x+", "+d.y);
-	console.log("event.x,event.y: "+d3.event.sourceEvent.x+", "+d3.event.sourceEvent.y);
-	console.log(d3.mouse(this));
-	// console.log(d3.touches("svg"));
-	// console.log(d3.touches("html"));
-	// console.log(d3.touches("rect"));
-	// console.log(d3.touches("this"));
-	// var eventx = d3.mouse(this)[0] ? d3.mouse(this)[0] : d.x;
-	// var eventy = d3.mouse(this)[1] ? d3.mouse(this)[1] : d.y;
-	//eventx += ((window.screen.availWidth - width)/2);
-	// var eventx = d3.mouse(this)[0] + ((window.screen.availWidth - width)/2); 
-	// var eventy = d3.mouse(this)[1];
-	// console.log("eventX: "+eventx);
-	// console.log("eventY: "+eventy);	
 	d3.select(this).transition()
 		.duration(3000)
 		.ease("cubic-out")
@@ -162,105 +140,36 @@ function dragend(d) {
 		// .attr("y", d.y += Math.min((height/8) * (dragdy/direction), 0));
 		.attr("x", d.x += 0)
 		.attr("y", d.y += 0);
-	//console.log(Math.min((width/5) * (dragdx/direction), width/3) + ", " + Math.min((width/5) * (dragdy/direction), width/3));
-<<<<<<< HEAD
-	//console.log(d3.select(document.elementFromPoint(eventx+40, eventy+40)));
-	//console.log(document.elementFromPoint(eventx+40, eventy+40));
-=======
-	tag = d3.select(document.elementFromPoint(d3.event.sourceEvent.x+40, d3.event.sourceEvent.y+40))[0][0].__data__.data;
-	 console.log(tag);
-	// console.log((width/5) * (dragdx/direction)+", "+(height/5) * (dragdy/direction));
-	// console.log(Math.min((width/5) * (dragdx/direction), width/4)+", "+Math.min((height/5) * (dragdy/direction), height/4));
->>>>>>> d4fb463465b65c96d6f676331a56b9560ec6bd3d
 	dragdx = 0;
 	dragdy = 0;
-	//tag = d3.select(document.elementFromPoint(eventx+40, eventy+40))[0][0].__data__.data;
-	//console.log(tag);
+
 	var tag = d3.select(document.elementFromPoint(d.y, d.x));
-	console.log(tag);
-	// var block  = d3.select(document.elementFromPoint(50, 50));
-	// console.log(block);
-	// var dog = d3.select(document.elementFromPoint(50, 100));
-	// console.log(dog);
-	//console.log(d3.event.sourceEvent);
-	//console.log(client.x+", "+client.y);
-	// console.log((width/5) * (dragdx/direction)+", "+(height/5) * (dragdy/direction));
-	// console.log(Math.min((width/5) * (dragdx/direction), width/4)+", "+Math.min((height/5) * (dragdy/direction), height/4));
 	var svg = d3.select("svg");
-	// d3.select("svg")
-	// 	.append("rect")
-	// 	.attr("width", 10)
-	// 	.attr("height", 10)
-	// 	.attr("x", event.changedTouches[0].clientX)
-	// 	.attr("y", event.changedTouches[0].clientY)
-	// 	.attr("fill", "white");
-	svg.append("rect")
-		.attr("width", 10)
-		.attr("height", 10)
-		.attr("x", d.x)
-		.attr("y", d.y)
-		.attr("fill", "green");
-	//this is at 0,0
-	// svg.append("rect")
-	// 	.attr("width", 10)
-	// 	.attr("height", 10)
-	// 	.attr("x", event.clientX)
-	// 	.attr("y", event.clientY)
-	// 	.attr("fill", "yellow");
-	//console.log(d3.event.sourceEvent.x+", "+d3.event.sourceEvent.y);
-	
-	console.log("here2");
-	
+		
 	// Remove the rectangle
 	var rect = d3.select("#draggable");
 	rect.transition()
 		.duration(1000)
 			.style("opacity", 0);
-	// var dragrect = d3.select("#draggable");
-	// dragrect.remove();
-	
-	console.log("here");
-	
+		
 	// Get the element position and detect the arc underneath
 	var x = event.clientX ? event.clientX : event.changedTouches[0].clientX;
 	var y = event.clientY ? event.clientY : event.changedTouches[0].clientY;
 	
 	setTimeout(function (){
-		// console.log("event:");
-		// console.log(rect);
 		rect.remove();
-		// console.log(event);
-		// console.log(x);
-		// console.log(y);
-		
 		var elementMouseIsOver = document.elementFromPoint(x, y),
-			patharc, tag;
-		//console.log(event.changedTouches[0].clientX);
-		//console.log(event.changedTouches[0].clientY);
-
-		// console.log(elementMouseIsOver);
-		// console.log(d3.select(elementMouseIsOver));	
+			patharc, tag;	
 		patharc = d3.select(elementMouseIsOver)[0][0].__data__;
-		// console.log(patharc);
 		if(patharc){
-			// console.log(patharc.data);	
 			tag = patharc.data;
 		}else{
 			alert("no tag selected");
 		}
-		// //Put back the circle???
-		// svg.append("circle")
-		// 	.attr("cy", y)
-		//     .attr("cx", x)
-		//     .attr("r", cdiameter/2)
-		// 	.attr("fill", "grey");
-
 		//Fade out and fade in feedback
 		fadeoutall();
 		feedback(tag);
     }, 1100);
-	
-
 }
 
 function fadeoutall() {
